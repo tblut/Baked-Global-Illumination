d09f16ea6c3a4d09c040d981ffc4215aed5c5d14
Timothy Blut <timothy.blut@rwth-aachen.de>
2018-06-06 22:02:11 +0200

    update Embree to version 3.2

--- embree-3.2.0.x64.windows/doc/CHANGELOG.md
+++ embree-3.2.0.x64.windows/doc/CHANGELOG.md
@@ -1,8 +1,26 @@
 Version History
 ---------------
 
+### New Features in Embree 3.2.0
+-   Improved watertightness of robust mode.
+-   Line segments, and other curves are now all contained in a single
+    BVH which improves performance when these are both used in a scene.
+-   Performance improvement of up to 20% for line segments.
+-   Bugfix to Embree2 to Embree3 conversion script.
+-   Added support for Hermite curve basis.
+-   Semantics of normal buffer for normal oriented curves has
+    changed to simplify usage. Please see documentation for details.
+-   Using GLFW and imgui in tutorials.
+-   Fixed floating point exception in static variable initialization.
+-   Fixed invalid memory access in rtcGetGeometryTransform for non-motion
+    blur instances.
+-   Improved self intersection avoidance for flat curves. Transparency rays
+    with tnear set to previous hit distance do not need curve radius
+    based self intersection avoidance as same hit is calculated again. For this
+    reason self intersection avoidance is now only applied to ray origin.
+
 ### New Features in Embree 3.1.0
--   Added new normal oriented curve primitive for ray tracing of grass-like
+-   Added new normal-oriented curve primitive for ray tracing of grass-like
     structures.
 -   Added new grid primitive for ray tracing tessellated and displaced surfaces
     in very memory efficient manner.
--- embree-3.2.0.x64.windows/include/embree3/rtcore_buffer.h
+++ embree-3.2.0.x64.windows/include/embree3/rtcore_buffer.h
@@ -14,10 +14,13 @@
 // limitations under the License.                                           //
 // ======================================================================== //
 
-#ifndef __RTC_BUFFER_ISPH__
-#define __RTC_BUFFER_ISPH__
+#pragma once
 
-#include "rtcore_device.isph"
+#include "rtcore_device.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
 
 /* Types of buffers */
 enum RTCBufferType
@@ -26,6 +29,7 @@ enum RTCBufferType
   RTC_BUFFER_TYPE_VERTEX           = 1,
   RTC_BUFFER_TYPE_VERTEX_ATTRIBUTE = 2,
   RTC_BUFFER_TYPE_NORMAL           = 3,
+  RTC_BUFFER_TYPE_TANGENT          = 4,
 
   RTC_BUFFER_TYPE_GRID                 = 8,
 
@@ -41,21 +45,23 @@ enum RTCBufferType
 };
 
 /* Opaque buffer type */
-typedef uniform struct RTCBufferTy* uniform RTCBuffer;
+typedef struct RTCBufferTy* RTCBuffer;
 
 /* Creates a new buffer. */
-RTC_API RTCBuffer rtcNewBuffer(RTCDevice device, uniform uintptr_t byteSize);
+RTC_API RTCBuffer rtcNewBuffer(RTCDevice device, size_t byteSize);
 
 /* Creates a new shared buffer. */
-RTC_API RTCBuffer rtcNewSharedBuffer(RTCDevice device, void* uniform ptr, uniform uintptr_t byteSize);
+RTC_API RTCBuffer rtcNewSharedBuffer(RTCDevice device, void* ptr, size_t byteSize);
 
 /* Returns a pointer to the buffer data. */
-RTC_API void* uniform rtcGetBufferData(RTCBuffer buffer);
+RTC_API void* rtcGetBufferData(RTCBuffer buffer);
 
 /* Retains the buffer (increments the reference count). */
 RTC_API void rtcRetainBuffer(RTCBuffer buffer);
 
-/* Releases the buffer handle (decrements the reference count). */
+/* Releases the buffer (decrements the reference count). */
 RTC_API void rtcReleaseBuffer(RTCBuffer buffer);
 
+#if defined(__cplusplus)
+}
 #endif

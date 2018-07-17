e3480d93ab9402c98b5380ef5d465fdac8ef7035
Timothy Blut <batzer94@gmail.com>
2018-07-17 20:51:20 +0200

    restructure project

--- assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /servers/krag/hdd-home3/tblut/Dev/baked-gi-project/assimp/contrib/zlib
-# Build directory: /servers/krag/hdd-home3/tblut/Dev/baked-gi-project/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

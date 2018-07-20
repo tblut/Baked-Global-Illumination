1fbbab8c94936d96868e6db4cc47f3ba9b6e8ebc
Timothy Blut <tblut@madariaga.informatik.rwth-aachen.de>
2018-07-20 09:23:11 +0200

    forgot the define again ...

--- libs/assimp-lean/contrib/zlib/CTestTestfile.cmake
+++ libs/assimp-lean/contrib/zlib/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/tblut/Dev/baked-gi-project/libs/assimp-lean/contrib/zlib
-# Build directory: /home/tblut/Dev/baked-gi-project/libs/assimp-lean/contrib/zlib
+# Source directory: /servers/krag/hdd-home3/tblut/Dev/baked-gi-project/libs/assimp-lean/contrib/zlib
+# Build directory: /servers/krag/hdd-home3/tblut/Dev/baked-gi-project/libs/assimp-lean/contrib/zlib
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
